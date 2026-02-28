e6fa7744276e81ef79a5add2ca0b74e3729f22a5
hoangndst <hoangndst@gmail.com>
2026-02-28 13:47:27 -0600

    chore: 121

---
+++ 150/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+
+from typing import List
+
+class Solution:
+    # two pointers
+    def maxProfit_0(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
+    
+    # dp
+    def maxProfit(self, prices: List[int]) -> int:
+        maxP = 0
+        minBuy = prices[0]
+
+        for sell in prices:
+            maxP = max(maxP, sell - minBuy)
+            minBuy = min(minBuy, sell)
+        
+        return maxP
