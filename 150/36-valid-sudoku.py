5a94976ada369304b8a741a2962ce326fb747bb1
hoangndst <hoangndst@gmail.com>
2026-02-28 13:37:11 -0600

    chore: 1680

---
+++ 150/36-valid-sudoku.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/valid-sudoku/
+
+from typing import List
+
+class Solution:
+    # solution 1: brute force :)
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for row in range(9):
+            seen = set()
+            for i in range(9):
+                if board[row][i] == ".":
+                    continue
+                if board[row][i] in seen:
+                    return False
+                seen.add(board[row][i])
+        
+        for col in range(9):
+            seen = set()
+            for i in range(9):
+                if board[i][col] == ".":
+                    continue
+                if board[i][col] in seen:
+                    return False
+                seen.add(board[i][col])
+
+        for square in range(9):
+            seen = set()
+            for i in range(3):
+                for j in range(3):
+                    row = (square // 3) * 3 + i
+                    col = (square % 3) * 3 + j
+                    if board[row][col] == ".":
+                        continue
+                    if board[row][col] in seen:
+                        return False
+                    seen.add(board[row][col])
+
+        return True
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from litcode!")
+
+
+if __name__ == "__main__":
+    main()
