4c75f67c2dbba11d397052b3cd731f5965e43e95
hoangndst <hoangndst@gmail.com>
2026-03-17 17:50:41 -0500

    chore: 75

--- 75/226-invert-binary-tree.py
+++ 75/226-invert-binary-tree.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/invert-binary-tree/
 
+from collections import deque
 from typing import Optional
 
 # Definition for a binary tree node.
@@ -18,4 +19,17 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
 
+        return root
+    # 2. bfs
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
         return root
